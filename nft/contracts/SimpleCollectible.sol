48edadbca740e058bae21bf9cdf00c094dfae57f
Perez <victorperezpro@hotmail.com>
2022-07-28 11:41:36 +0200

    bug2

--- nft/contracts/SimpleCollectible.sol
+++ nft/contracts/SimpleCollectible.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract SimpleCollectible is ERC721 {
     uint256 public tokenCounter;
 
-    constructor() public ERC721("Dogie", "DOG") {
+    constructor() public ERC721("Collection Test", "TEST") {
         tokenCounter = 0;
     }
 
--- nft/scripts/simple_collectible/create_collectible.py
+++ nft/scripts/simple_collectible/create_collectible.py
@@ -2,7 +2,7 @@
 from brownie import SimpleCollectible, accounts, network, config
 from scripts.helpful_scripts import OPENSEA_FORMAT
 
-sample_token_uri = "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+sample_token_uri = "https://ipfs.io/ipfs/Qma8NiFp6irNQJMhm7rYZrtPp2VqicejtL4LJGCEkXKkRs?filename=4-MARS.json"
 
 
 def main():
